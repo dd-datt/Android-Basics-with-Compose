ca7bc9c4c6b6a584cd4d8bd2bfa5a73d4f160d0a
Nguyen Van Dat <vdat319@gmail.com>
2024-06-20 17:48:49 +0700

    làm theo trên mạng nhưng ko được

--- pathway3-build-a-basic-layout/Composequadrant/app/src/main/java/com/example/composequadrant/MainActivity.kt
+++ pathway3-build-a-basic-layout/Composequadrant/app/src/main/java/com/example/composequadrant/MainActivity.kt
@@ -8,8 +8,8 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -18,7 +18,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.composequadrant.ui.theme.ComposeQuadrantTheme
@@ -29,104 +28,71 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ComposeQuadrantTheme {
-                ComposeQuadrant()
+                QuadrantApp()
             }
         }
     }
 }
 
+@Composable
+fun QuadrantApp() {
+    Column(modifier = Modifier.fillMaxWidth()) {
+        Row(modifier = Modifier.weight(1f)) {
+            CardInfor(
+                title = stringResource(id = R.string.text_composable),
+                description = stringResource(id = R.string.sub_txt1),
+                backgrColor = Color(0xFFEADDFF),
+                modifier = Modifier.weight(1f)
+
+            )
+            CardInfor(
+                title = stringResource(id = R.string.image_composable),
+                description = stringResource(id = R.string.sub_image),
+                backgrColor = Color(0xFFD0BCFF),
+                modifier = Modifier.weight(1f)
+            )
+        }
+        Row(modifier = Modifier.weight(1f)) {
+            CardInfor(
+                title = stringResource(id = R.string.row_composable),
+                description = stringResource(id = R.string.sub_row),
+                backgrColor = Color(0xFFB69DF8),
+                modifier = Modifier.weight(1f)
+
+            )
+            CardInfor(
+                title = stringResource(id = R.string.column_composable),
+                description = stringResource(id = R.string.sub_column),
+                backgrColor = Color(0xFFF6EDFF),
+                modifier = Modifier.weight(1f)
+            )
+        }
+    }
+}
 
 @Composable
-fun ComposeQuadrant() {
-    Column(modifier = Modifier.fillMaxSize()) {
-        Row(modifier = Modifier.weight(1f)) {
-            Column(
-                modifier = Modifier
-                    .weight(1f)
-                    .fillMaxHeight()
-                    .background(color = Color(0xFFEADDFF))
-                    .padding(9.dp),
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                Text(
-                    text = stringResource(id = R.string.text_composable),
-                    fontWeight = FontWeight.Bold,
-                    textAlign = TextAlign.Justify, modifier = Modifier.padding(16.dp)
-                )
-                Text(
-                    text = stringResource(id = R.string.sub_txt1),
-                    textAlign = TextAlign.Justify,
+fun CardInfor(
+    title: String,
+    description: String,
+    backgrColor: Color,
+    modifier: Modifier
+) {
+    Column(
+        modifier = Modifier
+            .padding(16.dp)
+            .fillMaxSize()
+            .background(backgrColor),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
 
-                    )
-            }
-            Column(
-                modifier = Modifier
-                    .weight(1f)
-                    .fillMaxHeight()
-                    .background(color = Color(0xFFD0BCFF))
-                    .padding(9.dp),
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                Text(
-                    text = stringResource(id = R.string.image_composable),
-                    fontWeight = FontWeight.Bold,
-                    textAlign = TextAlign.Justify, modifier = Modifier.padding(16.dp)
-                )
-                Text(
-                    text = stringResource(id = R.string.sub_image),
-                    textAlign = TextAlign.Justify,
-
-                    )
-            }
-        }
-        Row(modifier = Modifier.weight(1f)) {
-            Column(
-                modifier = Modifier
-                    .weight(1f)
-                    .fillMaxHeight()
-                    .background(color = Color(0xFFB69DF8))
-                    .padding(9.dp),
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                Text(
-                    text = stringResource(id = R.string.row_composable),
-                    fontWeight = FontWeight.Bold,
-                    textAlign = TextAlign.Justify, modifier = Modifier.padding(16.dp)
-                )
-                Text(
-                    text = stringResource(id = R.string.sub_row),
-                    textAlign = TextAlign.Justify,
-
-                    )
-            }
-            Column(
-                modifier = Modifier
-                    .weight(1f)
-                    .fillMaxHeight()
-                    .background(color = Color(0xFFF6EDFF))
-                    .padding(9.dp),
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                Text(
-                    text = stringResource(id = R.string.column_composable),
-                    fontWeight = FontWeight.Bold,
-                    textAlign = TextAlign.Justify, modifier = Modifier.padding(16.dp)
-                )
-                Text(
-                    text = stringResource(id = R.string.sub_column),
-                    textAlign = TextAlign.Justify,
-
-                    )
-            }
-        }
-
+        ) {
+        Text(
+            text = title,
+            modifier = Modifier.padding(bottom = 16.dp),
+            fontWeight = FontWeight.Bold
+        )
+        Text(text = description)
     }
-
-
 }
 
 
@@ -134,6 +100,6 @@ fun ComposeQuadrant() {
 @Composable
 fun GreetingPreview() {
     ComposeQuadrantTheme {
-        ComposeQuadrant()
+        QuadrantApp()
     }
 }
